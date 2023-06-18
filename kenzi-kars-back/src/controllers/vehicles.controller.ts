f8e388df9071fa2905cfcb3fde62c7ad19d4ffd4
eduschwert <eduardoschwert@yahoo.com.br>
2023-06-18 20:49:49 -0300

    Implement search filters and sorting options

--- kenzi-kars-back/src/controllers/vehicles.controller.ts
+++ kenzi-kars-back/src/controllers/vehicles.controller.ts
@@ -12,7 +12,6 @@ import createVehicleService from "../services/vehicles/createVehicle.service";
 import deleteVehicleService from "../services/vehicles/deleteVehicle.service";
 import updateVehicleService from "../services/vehicles/updateVehicle.service";
 import listVehiclesService from "../services/vehicles/listVehicles.service";
-import listUserVehiclesService from "../services/users/listUserVehicles.service";
 
 const createVehicleController = async (
   req: Request,
@@ -58,11 +57,77 @@ const listVehiclesController = async (req: Request, res: Response) => {
     }
   }
 
+  let brand: string | undefined = req.query.brand as string | undefined;
+  let model: string | undefined = req.query.model as string | undefined;
+  let color: string | undefined = req.query.color as string | undefined;
+  let year: string | undefined = req.query.year as string | undefined;
+
+  let fuel: number | undefined = req.query.fuel
+    ? parseInt(req.query.fuel as string, 10)
+    : undefined;
+  if (Number.isNaN(fuel)) {
+    fuel = undefined;
+  }
+  if (fuel !== undefined) {
+    fuel = fuel > 0 && fuel < 4 ? fuel : undefined;
+  }
+
+  let minMileage: number | undefined = req.query.minMileage
+    ? parseInt(req.query.minMileage as string, 10)
+    : undefined;
+  if (Number.isNaN(minMileage)) {
+    minMileage = undefined;
+  }
+
+  let maxMileage: number | undefined = req.query.maxMileage
+    ? parseInt(req.query.maxMileage as string, 10)
+    : undefined;
+  if (Number.isNaN(maxMileage)) {
+    maxMileage = undefined;
+  }
+
+  let minPrice: number | undefined = req.query.minPrice
+    ? parseInt(req.query.minPrice as string, 10)
+    : undefined;
+  if (Number.isNaN(minPrice)) {
+    minPrice = undefined;
+  }
+
+  let maxPrice: number | undefined = req.query.maxPrice
+    ? parseInt(req.query.maxPrice as string, 10)
+    : undefined;
+  if (Number.isNaN(maxPrice)) {
+    maxPrice = undefined;
+  }
+
+  let orderBy: "price" | "year" | "mileage" | undefined;
+
+  if (typeof req.query.orderBy === "string") {
+    const orderByQueryParam: string = req.query.orderBy;
+    if (
+      orderByQueryParam === "price" ||
+      orderByQueryParam === "year" ||
+      orderByQueryParam === "mileage"
+    ) {
+      orderBy = orderByQueryParam;
+    }
+  }
+
   const baseUrl: string = `${req.protocol}://${req.get("host")}${req.baseUrl}`;
 
   const vehicles: TPaginationResult = await listVehiclesService(
     perPage,
     page,
+    brand,
+    model,
+    color,
+    year,
+    fuel,
+    minMileage,
+    maxMileage,
+    minPrice,
+    maxPrice,
+    orderBy,
     baseUrl
   );
 
